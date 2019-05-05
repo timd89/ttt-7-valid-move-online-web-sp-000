5ac2206fa71f8ad45d358babf80e304b09253450
Timothy Dick <timothydicknz@gmail.com>
2019-05-05 21:36:22 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,10 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?
+  board = [nil " ", " ", " ", " ", " ", " ", " ", " ", " "]
+else between?
+  (0, 8)
+end
+
+  
