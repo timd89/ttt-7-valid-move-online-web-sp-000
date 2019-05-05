696e3cbf7f8dea687e503e4a6879d4cf4bd5c3f5
Timothy Dick <timothydicknz@gmail.com>
2019-05-05 21:34:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,7 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?
+  board = [nil " ", " ", " ", " ", " ", " ", " ", " ", " "]
+  
+  
